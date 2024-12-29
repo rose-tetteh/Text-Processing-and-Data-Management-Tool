ca17e6d38c9a1de3d6d13423c69c81f29df84f62
rose-tetteh <rose.tetteh@amalitech.com>
2024-12-29 22:41:44 +0000

    feat: add error handling for file operations1

--- src/main/java/com/labs/textprocessor/regex/TextValidator.java
+++ src/main/java/com/labs/textprocessor/regex/TextValidator.java
@@ -1,16 +1,29 @@
 package com.labs.textprocessor.regex;
 
+/**
+ * The type Text validator.
+ */
 public class TextValidator {
     private static final int MAX_TEXT_LENGTH = 1_000_000;
 
-    //Validates the text(not null, not empty,  and within max length)
+    /**
+     * Is valid text boolean.
+     * Validates the text(not null, not empty,  and within max length)
+     * @param text the text
+     * @return the boolean
+     */
     public boolean isValidText(String text){
         return text != null && !text.trim().isEmpty() && text.length() <= MAX_TEXT_LENGTH;
 
     }
 
-    //Validates the file path(Should be a .txt  file and not contain invalid characters)
+    /**
+     * Is valid file path boolean.
+     * Validates the file path(Should be a .txt  file and not contain invalid characters)
+     * @param path the path
+     * @return the boolean
+     */
     public boolean isValidFilePath(String path){
-        return path != null && path.endsWith(".txt") && !path.contains("...");
+        return path == null || !path.endsWith(".txt") || path.contains("...");
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.labs.textprocessor.textprocessor2 {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires com.almasb.fxgl.all;
+    requires java.logging;
 
     opens com.labs.textprocessor to javafx.fxml;
     exports com.labs.textprocessor;
